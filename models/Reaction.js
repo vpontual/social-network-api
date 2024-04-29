8c9a89fa0669f44bf00fcbe2c2cab2cf98ee3a27
veepee <66334129+vpontual@users.noreply.github.com>
2024-04-29 17:24:13 -0400

    updated Reaction model

--- models/Reaction.js
+++ models/Reaction.js
@@ -1,15 +1,31 @@
 const mongoose = require("mongoose");
 
-const reactionSchema = new mongoose.Schema({
-  thought: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "thought",
+const reactionSchema = new mongoose.Schema(
+  {
+    reactionId: {
+      type: mongoose.Schema.Types.ObjectId,
+      default: () => new Types.ObjectId(),
+    },
+    reactionBody: {
+      type: String,
+      required: true,
+      maxlength: 280,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
   },
-  text: {
-    type: String,
-  },
-});
+  {
+    toJSON: {
+      getters: true,
+    },
+    id: false,
+  }
+);
 
-const Reaction = mongoose.model("Reaction", reactionSchema);
-
-module.exports = Reaction;
+module.exports = reactionSchema;
