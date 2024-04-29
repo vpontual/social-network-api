e5f4ba741065ad5be990c298a9769a374924eb2e
veepee <66334129+vpontual@users.noreply.github.com>
2024-04-29 17:47:06 -0400

    updated the server.js to mount the /api route

--- routes/api/reactionsRoutes.js
+++
@@ -1,6 +0,0 @@
-// Reaction Routes
-
-// POST routes for reactions
-route.post("/reactions", addReaction);
-// DELETE routes for reactions
-route.delete("/reactions/:reactionId", removeReaction);
--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use("/", routes);
+app.use("/api", routes);
 
 db.once("open", () => {
   app.listen(port, () => {
