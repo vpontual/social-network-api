0ca822a95a0406a00811ccea74d5c246d81ccb95
veepee <66334129+vpontual@users.noreply.github.com>
2024-04-29 17:21:58 -0400

    updated Thought model

--- models/Thought.js
+++ models/Thought.js
@@ -1,15 +1,36 @@
-const mongoose = require("mongoose");
+const { Schema, model } = require("mongoose");
+const reactionSchema = require("./Reaction");
 
-const thoughtSchema = new mongoose.Schema({
-  reaction: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "reaction",
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      minlength: 1,
+      maxlength: 280,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    reactions: [reactionSchema],
   },
-  text: {
-    type: String,
-  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+thoughtSchema.virtual("reactionCount").get(function () {
+  return this.reactions.length;
 });
 
-const Thought = mongoose.model("Thought", thoughtSchema);
+const Thought = model("Thought", thoughtSchema);
 
 module.exports = Thought;
